61d9697266c4e7caf1040b214b6972804e2e9c52
DAVID SUGIARTO <s160418025@student.ubaya.ac.id>
2021-08-14 13:20:44 +0700

    toast

--- app/src/main/java/com/ubaya/week1advnatv/MainActivity.kt
+++ app/src/main/java/com/ubaya/week1advnatv/MainActivity.kt
@@ -2,12 +2,13 @@ package com.ubaya.week1advnatv
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        //print("FirstFeature")
+        Toast.makeText(MainActivity(), "This is my Toast message!",
+            Toast.LENGTH_LONG).show();
     }
 }
